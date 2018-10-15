828e5c18430ce544ce3aec98dfa6bb5e115b29f5
iannaiian <ianjosef.rivera@ymail.com>
2018-10-16 06:24:23 +0800

    added slick slider

--- js/myscript.js
+++ js/myscript.js
@@ -18,3 +18,14 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 <!--End of Tawk.to Script-->
+
+
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
